83ec12181bb2c9eeb04bf0cedcb35ba5efe11645
Abel Kidane Haile <65540326+AbelKidane-abita@users.noreply.github.com>
2023-06-10 01:14:30 +0400

    A

--- PrototypeProject/Assets/CloneObject.cs
+++ PrototypeProject/Assets/CloneObject.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class CloneObject : MonoBehaviour
 {
+    public GameObject originalObject; // assign the original game object in the Inspector
+    public Transform MainMenu;
     // Start is called before the first frame update
-    void Start()
+    public void ClonemyObject()
     {
-        
+        GameObject clone = Instantiate(originalObject);
+        clone.transform.position = MainMenu.position;
+        clone.transform.rotation = Quaternion.identity;// set the rotation of the clone
+        clone.SetActive(true);
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
