b592eba2144276d292889a92524138d39a4db9df
Abel Kidane Haile <65540326+AbelKidane-abita@users.noreply.github.com>
2023-06-13 11:24:44 +0400

    A

--- PrototypeProject/Assets/VisibilityAndColor.cs
+++ PrototypeProject/Assets/VisibilityAndColor.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class VisibilityAndColor : MonoBehaviour
 {
+    public MeshRenderer GlassLiquid;
     // Start is called before the first frame update
-    void Start()
+    public void HandleButtonClick()
     {
-        
-    }
+        string buttonName = gameObject.name;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (buttonName == "Button1")
+        {
+            // Change the color of the target Renderer for Button 1
+            targetRenderer.material.color = Color.red;
+        }
+        else if (buttonName == "Button2")
+        {
+            // Change the color of the target Renderer for Button 2
+            targetRenderer.material.color = Color.blue;
+        }
+        // Add more conditions for other buttons
     }
 }
+
